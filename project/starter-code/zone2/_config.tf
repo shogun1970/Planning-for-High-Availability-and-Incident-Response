1b65bd94c26da81339ade33e807758dde1de447d
Glenn Burns <glenn.burns@gmail.com>
2022-06-01 06:03:38 -0500

    Update _config.tf

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-geburns-west"
+     bucket = "udacity-tf-glenn-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
