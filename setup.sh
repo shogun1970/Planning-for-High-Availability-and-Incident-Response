3910c592735b00b99f54754829c909d4916e7d5e
Glenn Burns <glenn.burns@gmail.com>
2022-06-01 06:18:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,23 @@ git config --global user.email "glenn.burns@gmail.com"
 git config --global core.editor "vim"
 
 ## 
+## helm setup
+
+export VERIFY_CHECKSUM=false
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+
+## terraform
+
+wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
+unzip terraform_1.0.7_linux_amd64.zip
+mkdir ~/bin
+mv terraform ~/bin
+export TF_PLUGIN_CACHE_DIR="/tmp"
+
+## kubectl
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
