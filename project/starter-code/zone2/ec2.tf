96cf50fdbb6457917cbd5b102f963df48bb87e93
Glenn Burns <glenn.burns@gmail.com>
2022-06-01 06:04:29 -0500

    Update ec2.tf

--- project/starter-code/zone2/ec2.tf
+++ project/starter-code/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0018dd77fd3fb571e"
+   aws_ami            = "ami-0b9810e97b00f45c4"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
